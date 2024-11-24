bdada5ed33a28ce75f03d038d3319be8834d1c2d
kmou424 <me@kmou424.moe>
2024-11-24 17:41:04 +0800

    chore(common): relocate debug flag to buildinfo

--- app/common/debug.go
+++ app/common/debug.go
@@ -1,13 +1,15 @@
-//go:build debug
-
 package common
 
 import (
+	"github.com/kmou424/sfcrypt/app/buildinfo"
 	"log/slog"
 	"os"
 )
 
 func init() {
+	if !buildinfo.Debug {
+		return
+	}
 	Logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
 		AddSource: true,
 		Level:     slog.LevelDebug,
